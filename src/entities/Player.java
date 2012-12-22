6cde32e8be3599838a7f30f95216dab0ad1d1980
KiierK <KiierK@Icarus>
2012-12-22 16:23:19 +0100

    change color

--- src/entities/Player.java
+++ src/entities/Player.java
@@ -24,7 +24,7 @@ public class Player extends Caracter
 
 	public void draw(Graphics g) 
 	{
-		g.setColor(Color.BLUE);
+		g.setColor(Color.RED);
 		g.fillRect(pixel.x, pixel.y, width, height);
 	}
 
